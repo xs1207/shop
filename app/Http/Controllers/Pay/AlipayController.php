7668657180a87cd8725c2d7822b06911671455e3
xs1207 <18237243730@163.com>
2019-01-15 19:43:43 +0800

    提交

--- app/Http/Controllers/Pay/AlipayController.php
+++ app/Http/Controllers/Pay/AlipayController.php
@@ -227,23 +227,10 @@ class AlipayController extends Controller
     {
 
 
-        header('Refresh:2;url=/order/list');
+//        header('Refresh:2;url=/order/list');
         echo "订单： ".$_GET['out_trade_no'] . ' 支付成功，正在跳转';
 
-        echo '<pre>';print_r($_GET);echo '</pre>';die;
-//        //验签 支付宝的公钥
-//        if(!$this->verify($_GET)){
-//            die('簽名失敗');
-//        }
-//
-//        //验证交易状态
-////        if($_GET['']){
-////
-////        }
-////
-//
-//        //处理订单逻辑
-//        $this->dealOrder($_GET);
+//        echo '<pre>';print_r($_GET);echo '</pre>';die;
     }
 
     /**
