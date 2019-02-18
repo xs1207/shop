ad682212d5dd846c8ecf0d8825777f8cf8108184
dukailong <dukailong@localhost.localdomain>
2019-02-18 05:49:54 +0000

    env

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -103,7 +103,7 @@ class WeixinController extends Controller
         //获取缓存
         $token = Redis::get($this->redis_weixin_access_token);
         if(!$token){        // 无缓存 请求微信接口
-            $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WEIXIN_APPID').'&secret='.env('WEIXIN_APPSECRET');
+            $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET');
             $data = json_decode(file_get_contents($url),true);
 
             //记录缓存
