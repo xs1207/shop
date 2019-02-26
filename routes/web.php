73e7a3d1d8dd61a0bc45fe26461c863139965c91
root <root@localhost.localdomain>
2019-02-26 07:28:37 +0000

    微信支付

--- routes/web.php
+++ routes/web.php
@@ -170,5 +170,7 @@ Route::get('/weixin/kefu/chat','Weixin\WeixinController@chatView');           //
 Route::get('/weixin/chat/get_msg','Weixin\WeixinController@getChatMsg');           //私聊
 Route::post('/weixin/chat/get_msg','Weixin\WeixinController@msg');           //私聊
 
-
+//微信支付
+Route::get('/weixin/pay/test/','Weixin\PayController@test');        //微信支付测试
+Route::post('/weixin/pay/notice/','Weixin\PayController@notice');      //微信支付通知回调
 
