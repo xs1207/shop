98e6ef35351279fd7a656cefc6ca22d44ec11e16
dukailong <dukailong@localhost.localdomain>
2019-02-27 06:34:41 +0000

    /

--- routes/web.php
+++ routes/web.php
@@ -172,6 +172,6 @@ Route::post('/weixin/chat/get_msg','Weixin\WeixinController@msg');           //�
 
 //微信支付
 Route::get('/weixin/pay/test/{order_id}','Weixin\PayController@test');        //微信支付测试
-Route::post('/weixin/pay/notice/','Weixin\PayController@notice');      //微信支付通知回调
-Route::get('/weixin/pay/wxpay/','Weixin\PayController@qrwx');      //微信支付视图层
+Route::post('/weixin/pay/notice','Weixin\PayController@notice');      //微信支付通知回调
+Route::get('/weixin/pay/wxpay','Weixin\PayController@qrwx');      //微信支付视图层
 
