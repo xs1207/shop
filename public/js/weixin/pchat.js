dcca85158627e80ef417ee1558fc8355fb1e7e7f
dukailong <dukailong@localhost.localdomain>
2019-03-07 08:35:13 +0000

    用户

--- public/js/weixin/pchat.js
+++ public/js/weixin/pchat.js
@@ -19,7 +19,7 @@ setInterval(function(){
                     $("#chat_div").append(msg_str);
                     $("#msg_pos").val(d.data.id)
                 }else{
-                    var msg_str = '<blockquote>' + '用户' +
+                    var msg_str = '<blockquote align="rigth">' + '用户' +
                         '<p>' + d.data.msg + '</p>' +
                         '</blockquote>';
 
